eebbcb554831a95803e93f1d34acbdb3439ae384
Mark <mark.bruekers@outlook.com>
2026-06-18 21:14:25 +0200

    jouw wens is mijn commando

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ballstopper/BallstopperIO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ballstopper/BallstopperIO.java
@@ -5,23 +5,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class BallstopperIO {
     private final Servo beunServo;
 
-    // Hood angle limits (degrees)
-    public static double MIN_HOOD_ANGLE = 0.0;
-    public static double MAX_HOOD_ANGLE = 300.0;
-
-    private static final double SERVO_RANGE_DEGREES = 300.0;
-
     public BallstopperIO(Servo beunServo) {
         this.beunServo = beunServo;
-    }
-
-    //set's position of hood between 15 en 40 degrees
-    public void setAngle(double hoodAngleDegrees) {
-        double servoAngleDegrees = Math.max(MIN_HOOD_ANGLE, Math.min(MAX_HOOD_ANGLE, hoodAngleDegrees));
-
-        double position = (servoAngleDegrees / SERVO_RANGE_DEGREES) * 2.0 - 1.0;
-
-        setPosition(position);
     }
 
     //set's position of servo between 0 and 1
